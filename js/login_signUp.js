69c67eee27a479d503154692d77154723afb1611
unknown <kamrunnesasamiya1997@gmail.com>
2024-10-21 23:50:56 +0600

    banner,nav,footer,search,calculate total price- these sections have been done

---
+++ js/login_signUp.js
@@ -0,0 +1,14 @@
+function login() {
+    const name = documen
+    show_details_modal.showModal();
+    // const dialog = document.getElementById('my_modal_3');
+    // dialog.innerHTML = `<div class="modal-box">
+    //     <h3 class="text-lg font-bold">Hello!</h3>
+    //     </div>`;
+
+    // ` 
+    // <button class="btn" onclick="my_modal_3.showModal()">open modal</button>
+    // <dialog id="my_modal_3" class="modal">
+    //     <h3 class="text-lg font-bold">Hello!</h3>
+    // </dialog>`
+}
